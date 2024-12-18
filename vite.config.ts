67ca0508e54b1219bdbaf99c20ae3b04eb180413
mrsamdev <sijosam1905@gmail.com>
2024-12-18 22:15:30 +0530

    update: added .netlify toml file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    assetsDir: "assets",
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
+});
